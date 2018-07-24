7f542f45b4f5cab8e1b5bef3834009e0a215be06
vshumilin97 <vshumilin1488@gmail.com>
2018-07-24 16:22:43 +0500

    Make this script fully abstract

--- day1/task1.py
+++ day1/task1.py
@@ -1,33 +1,38 @@
 # IDEAS:
 # Iterate through the whole object (non-effective)
-# ???
-# Make it like a JSON parse????
+
 import random
 names = {'Alex': 0, 'Bob': 0, 'John': 0, 'Cena': 0, 'Doe': 0, 'Michael': 0}
 boolean = {'True': 0, 'False': 0}
 dicts = []
 
 
-def build_list():
-    # bool(random.getrandbits(1)) May be useful
+def build_list(fname, sname, tname):
     for iterable in range(0, 10*10):
-        dicts.append({'id': iterable, 'success': random.choice(list(boolean.keys())),
-                      'name': random.choice(list(names.keys()))})
+        dicts.append({str(fname): iterable, str(sname): random.choice(list(boolean.keys())),
+                      str(tname): random.choice(list(names.keys()))})
 
 
 def count_enters():
-    build_list()
+    build_list('id', 'any', 'many')
     for each_dict in dicts:
-        names.update({each_dict['name']: names[each_dict['name']] + 1})
-        boolean.update({each_dict['success']: boolean[each_dict['success']] + 1})
+        for key in each_dict.keys():
+            try:
+                names.update({each_dict[key]: names[each_dict[key]] + 1})
+            except KeyError:
+                pass
+            try:
+                boolean.update({each_dict[key]: boolean[each_dict[key]] + 1})
+            except KeyError:
+                pass
 
 
 def updating_list():
     count_enters()
     keys = list(dicts[0].keys())[1:]
     dicts.clear()
-    success_dict = {'success': boolean}
-    names_dict = {'name': names}
+    success_dict = {keys[0]: boolean}
+    names_dict = {keys[1]: names}
     for rec in keys:
         try:
             appendables = success_dict[rec]
@@ -40,3 +45,4 @@ def updating_list():
 
 updating_list()
 print(dicts)
+
