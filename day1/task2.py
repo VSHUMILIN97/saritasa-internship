d482c511c30dcf7349ede138b96d615ad3b23939
vshumilin97 <vshumilin1488@gmail.com>
2018-07-24 16:27:59 +0500

    Change user message

--- day1/task2.py
+++ day1/task2.py
@@ -1,6 +1,8 @@
 # IDEAS
 # Merge?
 # Using update with IF statements
+import logging
+logging.basicConfig(level=logging.DEBUG)
 first = {'id': 15, 'uno': 1, 'bottle': 'neck'}
 second = {'id': 12, 'duos': 2, 'bottle': 'wine'}
 
@@ -9,7 +11,7 @@ def check_and_merge_my_dict(dict1, dict2):
     inner_keys = list(dict1.keys())
     for item in inner_keys:
         if dict2.get(item) is not None:
-            print('Warning, key -', item, 'is doubled')
+            logging.info('Warning, key - ' + item + ' is doubled')
     dict1.update(dict2)
     return dict1
 
