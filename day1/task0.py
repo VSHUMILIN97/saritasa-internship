9a99bd61d584b8d68e7a7f4c06f81cc82bb49a2d
vshumilin97 <vshumilin1488@gmail.com>
2018-07-25 13:25:18 +0500

    Rebuild transliteration algorithm
    The previous version of the algorithm used to be
    hardcoded harder than this. Added comments as well

--- day1/task0.py
+++ day1/task0.py
@@ -1,55 +1,118 @@
 import os
-sequence = {'A': 'А', 'B': 'Б', 'C': 'К',
-            'D': 'Д', 'E': 'Е', 'F': 'Ф', 'G': 'Г', 'H': 'Х',
-            'I': 'И', 'J': 'Ж', 'K': 'К', 'L': 'Л', 'M': 'М',
-            'N': 'Н', 'O': 'О', 'P': 'П', 'Q': 'КЬ', 'R': 'Р',
-            'S': 'C', 'T': 'Т', 'U': 'У', 'V': 'В', 'W': 'ВЬ',
-            'X': 'КС', 'Y': 'У', 'Z': 'З'}
+"""This script provide a function
+   that translates russian text to transliteration and vice versa.
+   Ex: “Эта строка будет подвержена транслитерации” ->
+   “Eta stroka budet podverzhena tranliteracii”
+"""
+SEQUENCE_TASK_0 = \
+        {'A': 'А',
+            'B': 'Б',
+            'C': 'К',
+            'D': 'Д',
+            'E': 'Е',
+            'F': 'Ф',
+            'G': 'Г',
+            'H': 'Х',
+            'I': 'И',
+            'J': 'Ж',
+            'K': 'К',
+            'L': 'Л',
+            'M': 'М',
+            'N': 'Н',
+            'O': 'О',
+            'P': 'П',
+            'Q': 'КЬ',
+            'R': 'Р',
+            'S': 'C',
+            'T': 'Т',
+            'U': 'У',
+            'V': 'В',
+            'W': 'ВЬ',
+            'X': 'КС',
+            'Y': 'У',
+            'Z': 'З',
+            'А': 'A',
+            'Б': 'B',
+            'В': 'V',
+            'Г': 'G',
+            'Д': 'D',
+            'Е': 'E',
+            'Ё': 'YO',
+            'Ж': 'J',
+            'З': 'Z',
+            'И': 'I',
+            'Й': 'YI',
+            'К': 'K',
+            'Л': 'L',
+            'М': 'M',
+            'Н': 'N',
+            'О': 'O',
+            'П': 'P',
+            'Р': 'R',
+            'С': 'S',
+            'Т': 'T',
+            'У': 'U',
+            'Ф': 'F',
+            'Х': 'H',
+            'Ч': 'CH',
+            'Ц': 'CE',
+            'Ш': 'SH',
+            'Щ': 'SH',
+            'Э': 'E',
+            'Ю': 'UY',
+            'Я': 'YA',
+            'Ь': '',
+            'Ъ': '',
+            'Ы': 'II'}
 
-reversed_sequence = {'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E',
-                     'Ё': 'YO', 'Ж': 'J', 'З': 'Z', 'И': 'I', 'Й': 'YI', 'К': 'K',
-                     'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R',
-                     'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'H', 'Ч': 'CH',
-                     'Ц': 'CE', 'Ш': 'SH', 'Щ': 'SH', 'Э': 'E', 'Ю': 'UY', 'Я': 'YA',
-                     'Ь': '', 'Ъ': '', 'Ы': 'II'}
+
+def setup_user_environment():
+    """
+    Function that collects current language
+    settings from the user's environment
+
+    :return: string value that contains current language settings
+    """
+    try:
+        user_language = os.getenv('LANG')
+    except OSError:
+        user_language = 'ru'
+    return user_language
+
+
+def get_string_from_user(current_language):
+    """
+    Function which purpose is to collect prompt from user
+
+    :param current_language: string value that receive language settings
+    :returns:
+        string: string value that contains user prompt for the transliteration
+    """
+    if current_language.startswith('ru'):
+        user_prompt = input('Введите строку на английском или русском языке: ')
+    else:
+        user_prompt = input('Enter a prompt in English or Russian: ')
+    return user_prompt
 
 
 def transliteration():
-    try:
-        get_language = os.getenv('LANG')
-    except OSError:
-        get_language = 'ru'
-    if get_language.startswith('ru'):
-        try:
-            input_from_user = input('Введите строку на английском или русском языке: ')
-        except IOError:
-            input_from_user = 'Стандартная строка для перевода'
-    else:
-        try:
-            input_from_user = input('Enter a prompt in English or Russian: ')
-        except IOError:
-            input_from_user = 'Default prompt for the translation'
-    #
+    """
+    Function that transliterate from eng to rus and vice versa.
+    NOTE: There is a global dictionary that contains prepared data.
+
+    :returns:
+        string: string to output
+    """
     output_sring = ''
-    for each_char in input_from_user:
-        try:
-            rus_char = sequence[each_char.upper()]
-            if each_char.islower():
-                output_sring += rus_char.lower()
-            else:
-                output_sring += rus_char
-        except KeyError:
-            try:
-                eng_char = reversed_sequence[each_char.upper()]
-                if each_char.islower():
-                    output_sring += eng_char.lower()
-                else:
-                    output_sring += eng_char
-            except KeyError:
-                output_sring += each_char
+    for each_char in get_string_from_user(setup_user_environment()):
+        if each_char.upper() in SEQUENCE_TASK_0.keys():
+            output_sring += SEQUENCE_TASK_0[each_char.upper()].lower() \
+                if each_char.islower()\
+                else SEQUENCE_TASK_0[each_char.upper()].upper()
+        else:
+            output_sring += each_char
     return output_sring
 
 
 if __name__ == '__main__':
     print(transliteration())
-
