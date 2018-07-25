070c6f1d23d2b3759140244b8785e6fe1a7596c7
vshumilin97 <vshumilin1488@gmail.com>
2018-07-25 18:01:27 +0500

    Save to continue work at home

--- day1/task1.py
+++ day1/task1.py
@@ -1,48 +1,51 @@
-import random
-entered_name = {'Alex': 0, 'Bob': 0, 'John': 0, 'Cena': 0, 'Doe': 0, 'Michael': 0}
-entered_boolean = {'True': 0, 'False': 0}
-list_with_dicts = []
+"""
+"""
+import csv
+import logging
+import argparse
 
 
-def build_list(f_name, s_name, t_name):
-    for enter_iterable in range(0, 10*10):
-        list_with_dicts.append({str(f_name): enter_iterable,
-                                str(s_name): random.choice(list(entered_boolean.keys())),
-                                str(t_name): random.choice(list(entered_name.keys()))
-                                })
+def parse_csv(path_to_file):
+    listing = []
+    try:
+        with open(path_to_file.path, 'r') as csvfile:
+            key_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            keys = next(key_reader)
+            for row in key_reader:
+                custom_dict = {}
+                for key in range(0, len(keys)):
+                    custom_dict.setdefault(keys[key], row[key])
+                listing.append(custom_dict)
+        return listing, keys
+    except IOError:
+        logging.error('Cannot reach the file')
+        exit(0)
 
 
-def count_enters():
-    build_list('id', 'Cany', 'Deny')  # TO-DO REFACTOR
-    for each_dict in list_with_dicts:
-        for appeared_key in each_dict.keys():
-            try:
-                entered_name.update({each_dict[appeared_key]: entered_name[each_dict[appeared_key]] + 1})
-            except KeyError:
-                pass
-            try:
-                entered_boolean.update({each_dict[appeared_key]: entered_boolean[each_dict[appeared_key]] + 1})
-            except KeyError:
-                pass
+def count_entries(generated_list, keys):
+    output_list = []
+    output_dict = {}
+    for each_dict in generated_list:
+        for key, val in each_dict.items():
+            output_dict.setdefault('key', key)
+            output_dict.setdefault('value', val)
+            output_dict.setdefault('count', 0)
+            output_list.append(output_dict)
+    for every_dict in output_list:
+        for same_dict in output_list:
+            if every_dict is same_dict:
+                every_dict['count'] += 1
+    return output_list
 
 
-def updating_list():
-    count_enters()
-    current_keys = list(list_with_dicts[0].keys())[1:]
-    list_with_dicts.clear()
-    link_boolean_dict = {current_keys[0]: entered_boolean}
-    link_names_dict = {current_keys[1]: entered_name}
-    for every_key in current_keys:
-        try:
-            appendables = link_boolean_dict[every_key]
-        except KeyError:
-            appendables = link_names_dict[every_key]
-        for every_key in appendables:
-            if appendables[every_key] > 1:
-                list_with_dicts.append({'key': every_key, 'value': every_key, 'count': appendables[every_key]})
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Process file path')
+    parser.add_argument('path', metavar='P', type=str,
+                        help='a string contains absolute path to file')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    updating_list()
-    print(list_with_dicts)
+    parse_result = parse_csv(get_arguments())
+    print(count_entries(parse_result[0], parse_result[1]))
 
