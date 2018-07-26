1d8d6700a0b66f53f5a7ce0461f23b35d485425b
vshumilin97 <vshumilin1488@gmail.com>
2018-07-26 15:47:50 +0500

    Improve error-handling behaviour

--- day1/task3.py
+++ day1/task3.py
@@ -25,7 +25,8 @@ def build_string(structure, user_prompt):
         user_prompt (str): provides text for Template Builder
 
     Returns:
-        The return value. True for success, False otherwise.
+        String: Prepared string from the users template
+        SystemExit: Code 1 in case of any mistake
     """
     example = Template(user_prompt.replace('{', '${'))
     try:
@@ -34,19 +35,21 @@ def build_string(structure, user_prompt):
                                              workplace=structure['workplace'],
                                              quote=structure['quote'],
                                              marriage=structure['marriage'])
-        print(prepared_string)
+        return prepared_string
     except KeyError as e:
         logging.error(f'Incorrect field → {e}')
+        return exit(1)
     except ValueError as e:
         logging.error(e)
+        return exit(1)
 
 
 if __name__ == '__main__':
     fake_object = Faker()
-    build_string({
+    print(build_string({
         'name': fake_object.name(),
         'age': int(18 + fake_object.random.random()*10),
         'workplace': fake_object.address(),
         'quote': fake_object.text(),
         'marriage': True,
-    }, input('Please proceed with the string format: '))
+    }, input('Please proceed with the string format: ')))
