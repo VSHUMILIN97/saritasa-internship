f2f253615e0b4e5daa9f037a5d06774a10d83203
vshumilin97 <vshumilin1488@gmail.com>
2018-07-26 15:13:25 +0500

    Improve second task behavior in case of exception

--- day1/task2.py
+++ day1/task2.py
@@ -8,20 +8,27 @@ second = {'id': 12, 'duos': 2, 'bottle': 'wine'}
 
 
 def check_and_merge_my_dict(dict1, dict2):
-    """
-    Function that checks whether there are common keys for
+    """Function that compares keys of both dicts
+
+    It checks whether there are common keys for
     two dictionaries, warning if so and merge them anyways
 
-    :param dict1: dictionary from user
-    :param dict2: dictionary from user
-    :return:
-        dict = dictionary that contains all the merged data
+    Args:
+        dict1 (dict): dictionary from user
+        dict2 (dict): dictionary from user
+
+    Returns:
+        dict: dictionary that contains all the merged data
     """
-    for key in dict1.keys():
-        if key in dict2.keys():
-            logging.info(f"Current key - '{key}' is doubled")
-    dict1.update(dict2)
-    return dict1
+    try:
+        for key in dict1.keys():
+            if key in dict2.keys():
+                logging.info(f"Current key - '{key}' is doubled")
+        dict1.update(dict2)
+        return dict1
+    except AttributeError:
+        logging.error('Provide script with dicts')
+        return exit(1)
 
 
 if __name__ == '__main__':
