1798fdc6475dda40657febd0c830ee4e11b26220
vshumilin97 <vshumilin1488@gmail.com>
2018-07-27 08:21:23 +0500

    Extend task1_tests with counter test
    Also, delete useless Mock dependencies, now code seems cleaner.

--- day1/task1_tests.py
+++ day1/task1_tests.py
@@ -8,8 +8,8 @@ Task:
 """
 import unittest
 from unittest import mock
-from day1.task1 import get_arguments, extending_data_with_keywords, \
-                       count_entries
+from day1.task1 import get_arguments, extending_data_with_keywords as ext_dt, \
+                       count_entries as c_e
 
 
 class CountMatchesTest(unittest.TestCase):
@@ -32,14 +32,11 @@ class CountMatchesTest(unittest.TestCase):
         Returns:
             The return value. True for success, False otherwise.
         """
-        mock_obj = mock.MagicMock(name='min',
-                                  return_value=extending_data_with_keywords)
-        func_from_mock = mock_obj()
         attribute_dict = {1: [1 in range(50)],
                           2: [1 in range(100)]}
-        self.assertIsInstance(func_from_mock(attribute_dict, [1, 2])[0], dict)
-        self.assertIsInstance(func_from_mock(attribute_dict, [1, 2]), list)
-        self.assertEqual(func_from_mock(attribute_dict, [1, 2])[0]['value'], 1)
+        self.assertIsInstance(ext_dt(attribute_dict, [1, 2])[0], dict)
+        self.assertIsInstance(ext_dt(attribute_dict, [1, 2]), list)
+        self.assertEqual(ext_dt(attribute_dict, [1, 2])[0]['value'], 1)
 
     def test_storage_data_in_dictionaries(self):
         """This test checks whether it is possible to storage data
@@ -48,10 +45,11 @@ class CountMatchesTest(unittest.TestCase):
         Returns:
             The return value. True for success, False otherwise.
         """
-        mock_obj = mock.MagicMock(name='min',
-                                  return_value=count_entries)
-        func_from_mock = mock_obj()
         prepared_data = [{1: 3, 2: 4}, {1: 5, 2: 6}]
         keys = [1, 2]
-        self.assertEqual(func_from_mock(prepared_data, keys)[1], keys)
-        self.assertEqual(func_from_mock(prepared_data, keys)[0][1], [3, 5])
+        self.assertEqual(c_e(prepared_data, keys)[1], keys)
+        self.assertEqual(c_e(prepared_data, keys)[0][1], [3, 5])
+
+    def test_count_return_true_value(self):
+        result = ext_dt({1: [1, 1, 1, 1, 1]}, [1])
+        self.assertEqual({'key': 1, 'value': 1, 'count': 5}, result[0])
