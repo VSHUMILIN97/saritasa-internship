be33c1e362d4acddb14d058578cf69735ef7b14c
vshumilin97 <vshumilin1488@gmail.com>
2018-07-27 09:57:15 +0500

    Delete mock dependencies and improve readability

--- day1/task3_tests.py
+++ day1/task3_tests.py
@@ -7,39 +7,27 @@ Task:
     Validate template strings for given context
 """
 import unittest
-from unittest import mock
-from day1.task3 import build_string
+from day1.task3 import build_string as b_str
 
 
 class ValidatorTest(unittest.TestCase):
 
     def test_validation_passes_with_all_params(self):
         """This test checks whether string is built or not
-
-        Returns:
-            The return value. True for success, False otherwise.
         """
-        mock_obj = mock.MagicMock(name='min',
-                                  return_value=build_string)
-        func_from_mock = mock_obj()
-        self.assertIsInstance(func_from_mock({'name': 'Let',
-                                              'age': 'down',
-                                              'workplace': 'you',
-                                              'quote': 'dirty',
-                                              'marriage': 'rabbit'},
-                                             '{name} me {age}'), str)
+        self.assertIsInstance(b_str({
+            'name': 'Let',
+            'age': 'down',
+            'workplace': 'you',
+            'quote': 'dirty',
+            'marriage': 'rabbit'},
+            '{name} me {age}'), str)
 
     def test_validation_exit_with_wrong_input(self):
         """This test check whether system shutdown with code 1 or
            raises an exception
-
-        Returns:
-            The return value. True for success, False otherwise.
         """
-        mock_obj = mock.MagicMock(name='min',
-                                  return_value=build_string)
-        func_from_mock = mock_obj()
         with self.assertRaises(SystemExit) as exit_manager:
-            func_from_mock({'context': 'me'}, '{context}')
+            b_str({'context': 'me'}, '{context}')
 
         self.assertEqual(exit_manager.exception.code, 1)
