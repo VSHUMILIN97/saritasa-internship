bc5c9c56a85d5c471d87f99708cb2dd092047a83
vshumilin97 <vshumilin1488@gmail.com>
2018-08-02 12:06:18 +0500

    Add cls matrix logic and add tests

--- day5/task2.py
+++ day5/task2.py
@@ -1,13 +1,14 @@
 """
 ADD MATRIX STUFF
 """
+import copy
 
 
 class MimicMatrix:
 
     def __init__(self, matrix):
-        self.matrix = matrix
-        self.m_size = (len(matrix), 0)
+        self.matrix = [x for x in matrix]
+        self.m_size = (len(self.matrix), 0)
         self.check_size()
 
     def check_size(self):
@@ -19,11 +20,12 @@ class MimicMatrix:
         if self.m_size[0] == 0:
             raise ValueError('Put dimensions to matrix')
         for matrix_index in range(len(self.matrix)):
-            for row_index in range(len(self.matrix[matrix_index])):
-                print(self.matrix[matrix_index][row_index])
-            print()
+            if len(self.matrix[matrix_index]) != \
+                    len(self.matrix[matrix_index - 1]):
+                raise ValueError('Rows are not the same size')
+        self.m_size = (len(self.matrix), len(self.matrix[0]))
 
-    def is_matrix_compatible(self, matrix):
+    def _is_matrix_compatible(self, matrix):
         """ Checks whether matrix are compatible
 
         Args:
@@ -38,9 +40,89 @@ class MimicMatrix:
             raise ValueError('Matrix instances are not the same size')
         return True
 
-    def __add__(self, other):
-        pass
+    def _is_matrix_mul_compatible(self, matrix):
+        if isinstance(matrix, int):
+            return False
+        if not isinstance(matrix, MimicMatrix):
+            raise TypeError('Not Matrix instance')
+        if self.m_size != tuple(reversed(matrix.m_size)):
+            raise TypeError('Matrix instances cannot be multiplied')
+        return True
+
+    def __add__(self, other_matrix):
+        if self._is_matrix_compatible(other_matrix):
+            rows, values = self.m_size
+            for row in range(rows):
+                for value in range(values):
+                    self.matrix[row][value] += other_matrix[row][value]
+        else:
+            raise TypeError('Unsupported operand type for instances'
+                            ' - MimicMatrix and int}')
+        return self.matrix
+
+    def __getitem__(self, index):
+        return self.matrix[index]
+
+    def __sub__(self, other_matrix):
+        if self._is_matrix_compatible(other_matrix):
+            rows, values = self.m_size
+            for row in range(rows):
+                for value in range(values):
+                    self.matrix[row][value] -= other_matrix[row][value]
+        else:
+            raise TypeError('Unsupported operand type for instances'
+                            ' - MimicMatrix and int}')
+        return self.matrix
+
+    def __getattr__(self, attr):
+        if self.__dict__.get(attr) is not None:
+            return self.__dict__[attr]
+        else:
+            raise AttributeError(f'There are no - {attr} in this instance')
+
+    def __mul__(self, other_matrix_or_num):
+        if self._is_matrix_mul_compatible(other_matrix_or_num):
+            other_matrix_or_num = list(zip(*other_matrix_or_num))
+            new_structure = []
+            st = []
+            counter = 0
+            rows, values = self.m_size
+            for row in range(rows):
+                for tuple_len in range(len(other_matrix_or_num)):
+                    for value in range(values):
+                        val = self.matrix[row][value] * other_matrix_or_num[tuple_len][value]
+                        counter += val
+                    new_structure.append(counter)
+                    counter = 0
+                st.append(copy.copy(new_structure))
+                new_structure.clear()
+            return st
+        else:
+            copyr = copy.copy(self.matrix)
+            rows, values = self.m_size
+            for row in range(rows):
+                for value in range(values):
+                    copyr[row][value] *= other_matrix_or_num
+            return copyr
+
+    def __pow__(self, number, modulo=None):
+        if type(number) is int:
+            matrix = None
+            columns, rows = self.m_size
+            if columns != rows:
+                raise TypeError('Unsupported matrix type (must be square)')
+            for _ in range(number - 1):
+                matrix = self.__mul__(MimicMatrix(self.matrix))
+            return matrix
+        else:
+            raise TypeError(f'Cannot involute on - {type(number)} type')
+
+    def transpose(self):
+        return list(zip(*self.matrix))
+
+    def __repr__(self):
+        return str(self.matrix)
 
 
-a = MimicMatrix([[1, 2, 3], [5, 6, 7]])
-a.check_size()
+if __name__ == '__main__':
+    pass
---
+++ day5/task2_tests.py
@@ -0,0 +1,109 @@
+import unittest
+from day5.task2 import MimicMatrix
+
+
+class MatrixTest(unittest.TestCase):
+
+    def test_matrix_is_same_row_size_and_not_null(self):
+        """ Test that checks whether matrix object raises Error
+            if its rows are not the same size or null
+        """
+        with self.assertRaises(ValueError) as cm:
+            MimicMatrix([[1, 2, 3], [5, 6]])
+        self.assertEqual(str(cm.exception), 'Rows are not the same size')
+        with self.assertRaises(ValueError) as cm:
+            MimicMatrix([])
+        self.assertEqual(str(cm.exception), 'Put dimensions to matrix')
+
+    def test_matrix_are_sub_add_compatible(self):
+        """ Test that checks whether matrix object raises Error
+            if they are not the same size
+        """
+        m1m = MimicMatrix([[1, 2, 4], [5, 7, 9]])
+        m2m = MimicMatrix([[1, 2, 5], [10, 14, -5]])
+        m3m = MimicMatrix([[1, 2], [4, 6]])
+        self.assertTrue(m1m._is_matrix_compatible(m2m))
+        with self.assertRaises(ValueError) as cm:
+            m1m._is_matrix_compatible(m3m)
+        self.assertEqual(str(cm.exception),
+                         'Matrix instances are not the same size')
+
+    def test_attributes_can_be_fetched_from_instance(self):
+        m3m = MimicMatrix([[1, 2], [4, 6]])
+        self.assertEqual(m3m.m_size, (2, 2))
+        with self.assertRaises(AttributeError) as cm:
+            print(m3m.spoon)
+        self.assertEqual(str(cm.exception),
+                         'There are no - spoon in this instance')
+
+    def test_matrix_addition_is_possible_and_correct(self):
+        """ Test checks whether it is possible to
+        """
+        m1m = MimicMatrix([[1, 2, 4],
+                           [5, 7, 9]])
+        m2m = MimicMatrix([[1, 2, 5],
+                           [10, 14, -5]])
+        obj = m1m + m2m
+        self.assertEqual([[2, 4, 9],
+                          [15, 21, 4]], obj)
+
+    def test_matrix_substruction_is_possible_and_correct(self):
+        m1m = MimicMatrix([[1, 2, 4],
+                           [5, 7, 9]])
+        m2m = MimicMatrix([[1, 2, 5],
+                           [10, 14, -5]])
+        obj = m1m - m2m
+        self.assertEqual([[0, 0, -1],
+                          [-5, -7, 14]], obj)
+
+    def test_multiplication_of_matrix_with_either_matrix_and_number(self):
+        """ Test checks whether non-square matrix can be multiplied by number
+            or another matrix
+        """
+        m3m = MimicMatrix([[1, 2, 3],
+                           [0, 1, 0]])
+        m4m = MimicMatrix([[1, 1],
+                           [0, 0],
+                           [2, 1]])
+        self.assertEqual(m3m*m4m, [[7, 4],
+                                   [0, 0]])
+        self.assertEqual(m3m*2, [[2, 4, 6],
+                                 [0, 2, 0]])
+
+    def test_square_matrix_multiplication(self):
+        """ Test checks whether square matrix multiple well"""
+        m1m = MimicMatrix([[1, 2],
+                           [2, 1]])
+        m2m = MimicMatrix([[1, 3],
+                           [4, 1]])
+        self.assertEqual(m1m*m2m,
+                         [[9, 5],
+                          [6, 7]])
+
+    def test_involute_matrix_with_bad_or_good_inputs(self):
+        """ Test checks whether it is possible to involute matrix"""
+        m3m = MimicMatrix([[2, 3],
+                           [3, 5]])
+        self.assertEqual(m3m**2, [[13, 21],
+                                  [21, 34]])
+        wrong_mm = MimicMatrix([[1, 2, 8],
+                                [3, 4, 5]])
+        with self.assertRaises(TypeError) as cm:
+            wrong_mm ** 2
+        self.assertEqual(str(cm.exception), 'Unsupported matrix type (must be square)')
+        with self.assertRaises(TypeError) as cm:
+            wrong_mm ** '12'
+        self.assertEqual(str(cm.exception), "Cannot involute on - <class 'str'> type")
+
+    def test_matrix_is_transposed(self):
+        """ Test checks whether transpose was successful"""
+        m3m = MimicMatrix([[1, 2, 3, 4],
+                          [5, 6, 7, 8],
+                           [9, 10, 11, 12]]).transpose()
+        self.assertEqual(m3m,
+                         [(1, 5, 9),
+                          (2, 6, 10),
+                          (3, 7, 11),
+                          (4, 8, 12)])
+
+
