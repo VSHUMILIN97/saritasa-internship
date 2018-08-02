141960e1d49ab93672f97edfe243781f651b250e
vshumilin97 <vshumilin1488@gmail.com>
2018-08-02 14:01:19 +0500

    Update docstrings for the sixth day task

--- day6/task.py
+++ day6/task.py
@@ -1,26 +1,26 @@
 """
     TO-DO:
     * Unit tests
-    * Infinite handle
 """
 import time
 from functools import wraps
 
+""" This function is stub for fetching argument which are
+    exceptions, retries and delay
+
+Args:
+    exceptions: The exception to check. may be a tuple of
+        exceptions to check.
+    retries: Number of times to try (not retry) before giving up.
+    delay: Initial delay between retries in seconds.
+
+Returns:
+    object: The result of decorated function by chain
+"""
+
 
 def retry(exceptions, retries=None, delay=3):
-    """ This function is stub for fetching argument which are
-        exceptions, retries and delay
-
-    Args:
-        exceptions: The exception to check. may be a tuple of
-            exceptions to check.
-        retries: Number of times to try (not retry) before giving up.
-        delay: Initial delay between retries in seconds.
-
-    Returns:
-        object: The result of decorated function by chain
-    """
-    def decorator_retry(func):
+    def d_retry(func):
         """ Real decorator which handle function arguments and
             result of its work.
 
@@ -32,15 +32,7 @@ def retry(exceptions, retries=None, delay=3):
         """
         @wraps(func)
         def f_retry(*args, **kwargs):
-            """ Decoy function, which place herself instead of original
 
-            Args:
-                *args: Any positional args from original function
-                **kwargs: Any kwargs from original function
-
-            Returns:
-                object: The result of original function
-            """
             user_retries, user_delay = retries, delay
             if user_retries is None:
                 user_retries = True
@@ -58,7 +50,7 @@ def retry(exceptions, retries=None, delay=3):
                     user_retries -= 1
             return func(*args, **kwargs)
         return f_retry
-    return decorator_retry
+    return d_retry
 
 
 if __name__ == '__main__':
